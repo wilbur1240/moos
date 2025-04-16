200fa720c641521d327acf0a439cf7603f0517a2
Laipi1240 <charles92421@gmail.com>
2025-04-16 16:51:15 +0800

    update environment.sh

--- environment.sh
+++ environment.sh
@@ -1,3 +1,5 @@
 export PATH=$PATH:/home/wilbur/moos_test/moos-ivp/bin
 export PATH=$PATH:/home/wilbur/moos_test/moos-ivp-wilbur/bin
 export PATH=$PATH:/home/wilbur/moos_test/moos-ivp/scripts
+export MOOS_INCLUDE_DIRS=/home/wilbur/moos/moos-ivp/MOOSCore/include
+export MOOS_LIB_DIR=/home/wilbur/moos/moos-ivp/MOOSCore/lib
