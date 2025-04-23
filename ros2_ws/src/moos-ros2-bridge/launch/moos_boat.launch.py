d3e1884504299eb4f2b9b9754c7fbaf989596668
Laipi1240 <charles92421@gmail.com>
2025-04-23 17:38:05 +0800

    update moos_boat launch files

---
+++ ros2_ws/src/moos-ros2-bridge/launch/moos_boat.launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+import os
+
+def generate_launch_description():
+    config_dir = os.path.join(
+        os.getenv('AMENT_PREFIX_PATH').split(':')[0],
+        'share',
+        'moos-ros2-bridge',
+        'config'
+    )
+    moos_mission_dir = '/home/wilbur/moos/moos-ivp-wilbur/missions/'
+    mission = 'lab_05/alpha'
+
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=['MOOSDB', os.path.join(moos_mission_dir, mission, 'alpha.moos')],
+            name='moosdb',
+            output='screen'
+        ),
+        Node(
+            package='moos-ros2-bridge',
+            executable='moos_ros2_bridge_main',
+            name='bridge',
+            output='screen',
+            arguments=[
+                os.path.join(config_dir, 'moos_boat.xml'),
+                os.path.join(config_dir, 'bridge.moos')
+            ]
+        ),
+    ])
